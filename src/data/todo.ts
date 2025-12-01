3d60dd0677530b42d99550eb231073d343663da6
二叉树树 <acofork@qq.com>
2025-12-01 19:10:14 +0800

    fix(todo): 修正缩进并添加新的待办事项
    添加新的已完成任务"anuneko.com米哈游AI聊天机器人"，同时修正前一项任务的缩进格式

--- src/data/todo.ts
+++ src/data/todo.ts
@@ -8,6 +8,7 @@ export const todoConfig: TodoConfig = {
         { content: "做视频：利用STUN在世界各地连接上你的电脑", completed: false  },
         { content: "做视频：Ventoy+FirPE使用（可能不会做）", completed: false },
 		{ content: "做视频：Cloudflare 利用Origin Rules 6转4 访问家里云", completed: false },
-{ content: "做视频：anuneko.com米哈游AI上手体验", completed: false },
+        { content: "做视频：anuneko.com米哈游AI上手体验", completed: false },
+        { content: "做Bot插件：anuneko.com米哈游AI聊天机器人", completed:true },
     ],
 };
